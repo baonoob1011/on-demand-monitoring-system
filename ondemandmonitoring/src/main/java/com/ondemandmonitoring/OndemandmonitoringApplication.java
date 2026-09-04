cec8e001592bef67f9e85db8e6a87b55393c8b98
baonoob1011 <trandinhbao222@gmail.com>
2026-09-04 21:27:41 +0700

    initial project

---
+++ ondemandmonitoring/src/main/java/com/ondemandmonitoring/OndemandmonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.ondemandmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndemandmonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndemandmonitoringApplication.class, args);
+	}
+
+}
---
+++ ondemandmonitoring/src/test/java/com/ondemandmonitoring/OndemandmonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ondemandmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndemandmonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
